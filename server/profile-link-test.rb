2b3d1746d46ed10819f5c27a2cb6779a1b1d8ba8
katherine84522 <katherine84522@gmail.com>
2023-02-09 00:29:17 -0500

    linkedin profile link fixed

--- server/profile-link-test.rb
+++ server/profile-link-test.rb
@@ -1,11 +1,10 @@
 require 'selenium-webdriver'
 
 options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--headless')
+
 
 driver = Selenium::WebDriver.for :chrome, options:options
 driver.get('https://www.linkedin.com/')
-
 
 
 email = driver.find_element(:xpath, '//*[@id="session_key"]')
@@ -18,17 +17,34 @@ sleep 1
 signinBtn = driver.find_element(:xpath, '//*[@id="main-content"]/section[1]/div/div/form/button')
 signinBtn.click
 
-driver.get("https://www.linkedin.com/in/rafiakhandaker/")
+driver.get("https://www.linkedin.com/in/rustyrazorblade/")
 
 sleep 3
-name = driver.find_element(:xpath, '//*[@id="ember29"]/div[2]/div[2]/div[1]/div[1]/h1').text
-
+nameDiv = driver.find_element(:class_name, 'pv-text-details__left-panel')
+name = nameDiv.find_element(:tag_name, 'h1').text
+                                   
 p name
 
-company = driver.find_element(:xpath, '//*[@id="ember29"]/div[2]/div[2]/ul/li/button/span/div').text
+jobsection = driver.find_elements(:css, 'section.artdeco-card.ember-view.relative.break-words.pb3.mt2')[3]
+
+
+job_list = jobsection.find_element(:tag_name, 'ul')
+
+
+jobTitles = job_list.find_element(:css, 'span.mr1.t-bold')
+
+
+job_title = jobTitles.find_element(:css, 'span.visually-hidden').text
+
+p job_title
+
+company_name = job_list.find_element(:css, 'span.t-14.t-normal')
+
+
+company = company_name.find_element(:css, 'span.visually-hidden').text
 
 p company
 
-sleep 10
+
 
 driver.quit
